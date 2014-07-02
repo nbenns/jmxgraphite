d285c645dbbe91de294f913b111f8ed16aa055c0
Nigel Benns <nbenns@loyalty.com>
2014-07-02 15:53:43 -0400

    move log entry to before action

--- src/main/groovy/jmxgraphite/JMXConnection.groovy
+++ src/main/groovy/jmxgraphite/JMXConnection.groovy
@@ -42,14 +42,13 @@ class JMXConnection extends Thread {
 					
 					String newJson = new JsonBuilder(c).toPrettyString()
 					
+					_LOG.info("Rewriting ${fname} with encrypted password.")
 					try {
 						f.withWriter('UTF-8') { it << newJson }
 					}
 					catch (IOException ex) {
 						_LOG.warn("Can't write to file ${fname}")
 					}
-						
-					_LOG.info("Rewriting ${fname} with encrypted password.")
 				}
 				else {
 					_LOG.warn("No password for JVM ${fname} -- skipping")
